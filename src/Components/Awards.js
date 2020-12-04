f4c1d46c9878bfb286ae3d4b26d5d646129bb15e
prestoncarroll <plutusinc1@yahoo.com>
2020-12-04 12:06:56 -0600

    react bootstrap columns set

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -6,10 +6,23 @@ const Awards = () => {
       <header style={{ fontSize: "35px", color: "white" }}>
         Awards and Acheivements
       </header>
-      <div style={{ width: "30%" }}>
+      <div style={{ width: "100%" }}>
         <p1 style={{ color: "white" }}>
           {" "}
-          <li style={{ width: "100%" }}> - IBM Design Thinking Practitioner</li>
+          <li style={{ width: "100%" }}>
+            {" "}
+            IBM - Enterprise Design Thinking Practitioner
+          </li>
+          <li style={{ width: "100%" }}>
+            {" "}
+            AWS - Certificate of Completion Introduction to Blockchain{" "}
+          </li>
+          <li style={{ width: "100%" }}>
+            {" "}
+            UCLA - Completion of full stack web development bootcamp{" "}
+          </li>
+          <li style={{ width: "100%" }}> UCLA - Best overall group project</li>
+          <li style={{ width: "100%" }}> UCLA - Most resourceful</li>
         </p1>
       </div>
     </div>
